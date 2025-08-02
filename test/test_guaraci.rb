96e243fab8ddd3c29700aedcf9e45a4c2b89f392
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-08-01 22:30:25 -0300

    add tests

--- test/test_guaraci.rb
+++ test/test_guaraci.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
 
 class TestGuaraci < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Guaraci::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_can_create_a_server
+    @server = Guaraci::Server.new do |req|
+      Guaraci::Response.ok
+    end
+
+    assert_instance_of Guaraci::Server, @server
+  end
+
+  def test_that_response_class_exists
+    @response = Guaraci::Response.ok
+    assert_instance_of Guaraci::Response, @response
+    assert_equal 200, @response.status
+  end
+
+  def test_that_all_classes_are_available
+    assert defined?(Guaraci::Server)
+    assert defined?(Guaraci::Request)
+    assert defined?(Guaraci::Response)
   end
 end
