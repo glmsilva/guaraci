8764e370153ddd1c50cd1269ab00f7aac023f9bc
Guilherme Silva <guilherme.gss@outlook.com.br>
2025-08-01 23:17:40 -0300

    fix rubocop offenses

--- test/guaraci/test_request.rb
+++ test/guaraci/test_request.rb
@@ -1,32 +1,45 @@
+# frozen_string_literal: true
+
 require_relative "../test_helper"
 
-class Guaraci::TestRequest < Minitest::Test
-  RequestMock = Struct.new(:method, :path, :read, :query)
+module Guaraci
+  class TestRequest < Minitest::Test
+    def setup
+      @params = { message: "Hello, Guaraci!" }
+      @request_mock = Minitest::Mock.new
 
-  def setup
-    @params = { message: "Hello, Guaraci!" }
-    @request_mock = RequestMock.new(method: "get", path: "mock/routing", query: "size=small&id=11", read: @params.to_json)
-    @object = Guaraci::Request.new(@request_mock)
-  end
+      @request_mock.expect(:read, @params.to_json)
+      @request_mock.expect(:method, "get")
+      @request_mock.expect(:path, "/mock/api")
+      @request_mock.expect(:query, "size=small&id=11")
 
-  def test_that_body_is_read
-    assert_equal @request_mock.read, @object.body
-  end
+      @object = Guaraci::Request.new(@request_mock)
+    end
 
-  def test_that_params_is_parsed
-    assert @object.params.is_a? Hash
-    assert_equal "Hello, Guaraci!", @object.params["message"]
-  end
+    def test_that_body_is_read
+      assert_equal @params.to_json, @object.body
+    end
 
-  def test_that_query_is_parsed
-    assert @object.query_params.is_a? Array
-    assert_equal [["size", "small"], ["id", "11"]], @object.query_params
-  end
+    def test_that_params_is_parsed
+      assert @object.params.is_a?(Hash)
+      assert_equal "Hello, Guaraci!", @object.params["message"]
+    end
 
-  def test_parse_params_with_error
-    @request_mock = RequestMock.new(read: "")
-    @object = Guaraci::Request.new(@request_mock)
+    def test_that_query_is_parsed
+      assert @object.query_params.is_a?(Array)
+      assert_equal [%w[size small], %w[id 11]], @object.query_params
+    end
 
-    assert @object.params.empty?
+    def test_parse_params_with_error
+      empty_mock = Minitest::Mock.new
+      empty_mock.expect(:read, "")
+      empty_mock.expect(:method, "get")
+      empty_mock.expect(:path, "/mock/api")
+      empty_mock.expect(:query_string, "") # caso necessário
+
+      object = Guaraci::Request.new(empty_mock)
+
+      assert object.params.empty?
+    end
   end
 end
